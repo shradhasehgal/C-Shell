1a1a909f9af2d344941554dc83257542bcb39001
Shradha <shradha.sgl@gmail.com>
2019-08-20 18:46:23 +0530

    echo & ls halfway

--- headers.h
+++ headers.h
@@ -5,6 +5,7 @@
 #include <sys/types.h>
 #include <sys/dir.h>
 #include <sys/param.h>
+#include <dirent.h>
 
 #define EXIT_FAILURE 1
 char HOST[256], USER[256], CWD[2048], HOME[2048];
--- ls.c
+++ ls.c
@@ -1,45 +1,102 @@
 #include "headers.h"
 
-void ls_blank(char *dirname)
+int is_hidden(char *filename)
 {
+    if(filename[0] == '.') return 1;
+    else return 0;
+}
 
+void long_listing(char *directory, char *filename)
+{
+    char *filepath = (char *)malloc(sizeof(char) * (strlen(directory) + strlen(filename)+1);
+    strcpy(filepath, directory);
+    strcpy(filepath, "/");
+    strcpy(filepath, filename);
+
+    struct stat name;
+    if(stat(filepath, &name) != 0)
+        perror("Error");     
+
+    else
+    {
+        struct group *gr = getgrgid(file_stat.st_gid);
+        struct passwd *pwd =  getpwuid(file_stat.st_uid);
+        mode_t p = name.st_mode;
+        char *mode = malloc(sizeof(char) * 9 + 1);
+        mode[0] = (p & S_IRUSR) ? 'r' : '-';
+        mode[1] = (p & S_IWUSR) ? 'w' : '-';
+        mode[2] = (p & S_IXUSR) ? 'x' : '-';
+        mode[3] = (p & S_IRGRP) ? 'r' : '-';
+        mode[4] = (p & S_IWGRP) ? 'w' : '-';
+        mode[5] = (p & S_IXGRP) ? 'x' : '-';
+        mode[6] = (p & S_IROTH) ? 'r' : '-';
+        mode[7] = (p & S_IWOTH) ? 'w' : '-';
+        mode[8] = (p & S_IXOTH) ? 'x' : '-';
+        mode[9] = '\0';
+        return mode;
+    }
+    free(filepath);
+
+}
+
+void ls_out(char *dir_name, int flag, int hidden)
+{
+    struct dirent **file;
+    int n = scandir(dir_name, &file, NULL, alphasort);
+    if(n < 0)
+    {
+        perror("Error");
+        return;
+    }
+
+    switch(flag)
+    {
+        case 0:
+            for(int i=0; i<n; i++) 
+            { 
+                if(hidden == 0 && !is_hidden(file[i]->d_name) || hidden == 1)
+                    printf("%s\t",file[i]->d_name); 
+                free(file[i]); 
+            }
+            printf("\n");
+            free(file);
+            break;
+    
+        case 1:
+            for(int i=0; i<n; i++)
+            {
+                if(hidden == 0 && !is_hidden(file[i]->d_name) || hidden == 1)
+                    long_listing(dir_name, file[i]);
+                free(file[i]);
+            }
+            break;
+    }
     
 }
 
-void ls_l(char *dir)
-{
-
-}
-
-void ls_a(char *dir)
-{
-
-}
-
-void ls_al(char *dir)
-{
-
-}
 
 
 void ls(char *command)
 {
-    int f1 = 0, f2 = 0, f3 = 0;
+    int f1 = 0;
     char *token = command;
 
+    token = strtok(NULL, " \n\t\r");
+
+    int hidden = 0;
     while (token != NULL)
     {
         if(!strcmp(token,"-l"))
             f1 = 1;
     
         else if(!strcmp(token,"-a"))
-            f2 = 1;
+            hidden = 1;
 
-        else if(!strcmp(token,"-al"))
-            f3 = 1;
+        else if(!strcmp(token,"-al") || !strcmp(token,"-la"))
+        {   
+            f1 = 1, hidden = 1;
+        }
         
-        else if(!strcmp(token,"-la"))
-            f3 = 1;
 
         else
         {
@@ -62,16 +119,8 @@ void ls(char *command)
             }
 
             else final_path = token;
-            
-            if(f1 && f2 || f3)
-                ls_al(final_path);
-            else if(f1)
-                ls_l(final_path);
-
-            else if(f2)
-                ls_a(final_path);
-
-            else ls_blank(final_path);
+        
+            ls_out(final_path, f1, hidden);
         }
 
         token = strtok(NULL, " \n\t\r");
